2c30789c89a48e6cdf454b26b746768ad609f1ad
merazul2755 <merazulehsan880@gmail.com>
2022-05-04 01:05:00 +0600

    2nd commit

--- index.js
+++ index.js
@@ -1,28 +1,35 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_PRODUCT}:${process.env.DB_PASS}@cluster0.ukdn9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run(){
+    try{
+        await client.connect();
+        const productCollection = client.db('').collection('');
 
-const uri = "mongodb+srv://Fresh-Mart:Ch3vlSvypC1UyscV@cluster0.ukdn9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("fresh-mart").collection("users");
-  console.log('db connected')
-  // perform actions on the collection object
-  client.close();
+    }finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("Running server");
 });
 
-
-app.get('/', (req, res) =>{
-    res.send('Running server');
-});
-
-app.listen(port, ()=>{
-    console.log('Listening to port', port);
+app.listen(port, () => {
+  console.log("Listening to port", port);
 });
