a84c9fa2b830b1bab0d1474621ea1cc6f67fba79
merazul2755 <merazulehsan880@gmail.com>
2022-05-06 18:44:20 +0600

    Manage

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
 
 app.use(cors());
 app.use(express.json());
@@ -17,7 +18,37 @@ const client = new MongoClient(uri, {
 async function run(){
     try{
         await client.connect();
-        const productCollection = client.db('').collection('');
+        const productCollection = client.db('products').collection('item');
+
+        app.get('/items', async (req,res)=>{
+          const query = {};
+          const cursour = productCollection.find(query);
+          const items = await cursour.toArray();
+          res.send(items);
+        });
+
+        app.get('/items/:id', async (req,res)=>{
+          const id = req.params.id;
+          const query = {_id : ObjectId(id)};
+          const item = await productCollection.findOne(query);
+          res.send(item);
+
+        });
+
+        app.put('/items/:id', async(req,res)=>{
+          const id = req.params.id;
+          const updateItem = req.body;
+          const filter = {_id : ObjectId(id)};
+          const option = {upsert : true};
+          const updateDoc = {
+            $set : updateItem
+          };
+          const result = await productCollection.updateOne(filter, updateDoc, option);
+          res.send(result)
+
+        });
+
+
 
     }finally{
 
