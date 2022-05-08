c8982e040ace75ce18544cd0ce6ed79823ddec91
merazul2755 <merazulehsan880@gmail.com>
2022-05-08 20:17:56 +0600

    update

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ async function run(){
     try{
         await client.connect();
         const productCollection = client.db('products').collection('item');
+        const orderCollection = client.db('products').collection('order');
 
+        //Show All Product
         app.get('/items', async (req,res)=>{
           const query = {};
           const cursour = productCollection.find(query);
@@ -35,6 +37,14 @@ async function run(){
 
         });
 
+        //Add Product
+        app.post('/items', async (req,res)=>{
+          const newProduct = req.body; 
+          const result = productCollection.insertOne(newProduct);
+          res.send(result);
+        });
+
+        //Update Quantity
         app.put('/items/:id', async(req,res)=>{
           const id = req.params.id;
           const updateItem = req.body;
@@ -47,6 +57,43 @@ async function run(){
           res.send(result)
 
         });
+
+        //Delete
+        app.delete('/items/:id', async (req,res)=>{
+          const id = req.params.id;
+          const query = {_id : ObjectId(id)};
+          const result = await productCollection.deleteOne(query);
+          res.send(result);
+
+        });
+
+        //order get
+        app.get('/order', async (req,res)=>{
+          const email = req.query.email;
+          const query = {email: email};
+          const cursour = orderCollection.find(query);
+          const item = await cursour.toArray();
+          res.send(item);
+        });
+
+        //Order
+        app.post('/order', async(req,res)=>{
+          const order = req.body;
+          const result =await orderCollection.insertOne(order);
+          res.send(result);
+        })
+        //Delete order
+        app.delete('/order/:id', async (req,res)=>{
+          const id = req.params.id;
+          const query = {_id : ObjectId(id)};
+          const result = await orderCollection.deleteOne(query);
+          res.send(result);
+
+        });
+
+        
+
+        
 
 
 
